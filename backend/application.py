8f2ffa9324b950931ecef59a2f5e038834329b33
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-11-03 19:58:02 -0400

    database setup

--- backend/application.py
+++ backend/application.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request, redirect, session
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 
-@app.route("/")
-def home():
-    return "home"
+@app.route("/testing")
+def testing():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM testing2"))
+        table = result.all()
+        result_dict = [row._asdict() for row in table]
+        for row in result_dict:
+            print(row)
+
+    return 0
 
 
-@app.route("/message", methods=["POST"])
+@app.route("/api/message", methods=["POST"])
 def message():
     print(request.json)
     return request.json
+
+
+@app.route("/login", methods=["GET", "POST"])
+def check_login():
+
+    return "True"
 
 
 if __name__ == "__main__":
