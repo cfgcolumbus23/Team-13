6e327e80964c2e085f42ffd578e967e9c82a5dba
e-savar <66883712+e-savar@users.noreply.github.com>
2023-11-03 16:16:57 -0400

    added react dom

--- goodwillfront/src/App.js
+++ goodwillfront/src/App.js
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './components/Homepage/Homepage.js';
-import Login from './components/Loginpage/Loginpage.js';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home/> } />
-        <Route path="/login" element={ <Login/> } />
       </Routes>
     </div>
   );
