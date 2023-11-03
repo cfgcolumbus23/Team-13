f5eef78c234d458c4d7e897d45a68912f4de3488
e-savar <66883712+e-savar@users.noreply.github.com>
2023-11-03 17:17:00 -0400

    login page css

--- goodwillfront/src/components/Loginpage/Loginpage.js
+++ goodwillfront/src/components/Loginpage/Loginpage.js
@@ -1,13 +1,20 @@
 import './Loginpage.css';
 function Loginpage() {
     return (
-      <div >
-        <div className="login-pane">
-          <input />
-          <input />
-          <button className="login-button">Login</button>
+      <div className="login-pane">
+        <h1 className='login-label'>User Login</h1>
+      <form>
+        <div className="input-field">
+          <div className='input-label'>Username</div>
+          <input type="text" id="username" />
         </div>
-      </div>
+        <div className="input-field">
+        <div className='input-label'>Password</div>
+          <input type="password" id="password" />
+        </div>
+        <button className="login-btn">Login</button>
+      </form>
+    </div>
     );
   }
   
