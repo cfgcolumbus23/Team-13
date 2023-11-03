8c4fc35cefa569b4c9f3904e75a409471ed56b3f
MarkW040 <148275256+MarkW040@users.noreply.github.com>
2023-11-03 18:22:00 -0400

    Committing messages.js

--- goodwillfront/src/components/Messagepage/messages.js
+++ goodwillfront/src/components/Messagepage/messages.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import './components.css';
 import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {useState} from 'react';
 
 import {
   MainContainer,
@@ -8,23 +9,36 @@ import {
   MessageList,
   Message,
   MessageInput,
+  MessageGroup,
 } from "@chatscope/chat-ui-kit-react";
+import { react } from "@babel/types";
 
 function messages() {
+  const [userData, setUserData] = useState({username: "", message: ""});
+
+  function handleInput(event) {
+    const {value} = event.target;
+    setUserData({...userData, "message": value});
+  }
+
+  const[messages, setMessages] = useState([]);
+
+  function handleSend(event) {
+    setMessages(...messages,messages.push(userData.message));
+  }
+
+
   return (
     <div style={{ position: "relative", height: "500px" }}>
       <MainContainer>
         <ChatContainer>
           <MessageList>
-            <Message
-              model={{
-                message: "Hello World",
-                sentTime: "now",
-                sender: "Testing123",
-              }}
-            />
+            
+            {messages.map((content, index)=>{return (<Message model={{sender: content.username, message: content.message}}></Message>)}) }
+
           </MessageList>
-          <MessageInput placeholder="Type message here" />
+          <MessageInput placeholder="Type message here" onChange="handleInput"/>
+          <button type="button" onClick="handleSend">Send</button>
         </ChatContainer>
       </MainContainer>
     </div> 
