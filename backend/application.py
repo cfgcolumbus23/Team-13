425be355e62dd4850c418fa7ca3c05c0ff25844c
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-11-03 20:52:30 -0400

    finished check login

--- backend/application.py
+++ backend/application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 from database import engine
 from sqlalchemy import text
+import json
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -24,10 +25,20 @@ def message():
     return request.json
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["POST"])
 def check_login():
+    dict = request.json
+    username = dict['username']
+    password = dict['password']
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM users"))
+        table = result.all()
+        result_dict = [row._asdict() for row in table]
+        for row in result_dict:
+            if (row['username'] == username and row['password'] == password):
+                return "True"
 
-    return "True"
+    return "False"
 
 
 if __name__ == "__main__":
