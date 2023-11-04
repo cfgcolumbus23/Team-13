729431fe292439462cc62449dfdef473b110aec0
christofer198 <mowaolorunsola@Mowas-MacBook-Air.local>
2023-11-03 23:51:47 -0400

    Merging in employee.js

--- goodwillfront/src/components/Employeepage/employee.js
+++ goodwillfront/src/components/Employeepage/employee.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import './employee.css';
+import { Sidebar, Menu, MenuItem} from "react-pro-sidebar";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import { Divider } from 'rsuite'; 
+import { blue } from '@mui/material/colors';
+import { SIZE } from 'rsuite/esm/utils/constants';
+//import '../node_modules/rsuite/dist/rsuite.min.css'; 
 
-const Employee = () => {
+//<h2 style= {{color: 'blue'}}>Wage</h2>
 
-    return (
-        <div class="content">
-        <div class="General"><h2>General</h2>
-            <h3>Name</h3>
-            <p>Display name</p>
-            <h3>Phone</h3>
-            <p>Display phone</p>
-            <h3>address</h3>
-            <p>Display address</p>
-            <h3>email</h3>
-            <p>Display email</p>
-            <h3>Employee ID</h3>
-            <p>Display ID</p>
-        </div>
-        <div class="job"><h2>Job</h2>
-            <h3>Job Title</h3>
-            <p>Display title</p>
-            <h3>Employment startdate</h3>
-            <p>Display employment start date</p>
-        </div><div class="pay">
-            <h3>Wage</h3>
-            <p>Display hourly wage</p>
-            <h3>Pay Schedule</h3>
-            <p>Display pay sched</p>
-            <h3>Hours worked</h3>
-            <p>Display hours worked in pay period</p>
-        </div></div>
-    )
-}
 
-export default Employee;
+const employee = () => { 
+  
+      return ( 
+        
+        <div style={{ margin: 10 }}> 
+        <h1 style={{marginInlineStart: 100}}>Name  |  Job  |  Compensation | Schedule | Address  |   
+Email</h1>
+          <h2 style ={{marginRight: 100}}>Wage</h2>
+         
+          <h2 style ={{marginRight: 100}}>Hours</h2>
+          
+        
+              
+          </div> 
+      ); 
+  
+} 
+export default employee;
+
+
+
+
+
