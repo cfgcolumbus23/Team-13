adc1b6207f56257b5202d632d62da5e5ed06d83d
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-11-04 06:14:15 -0400

    connect jobs to db

--- goodwillfront/src/components/Homepage/Homepage.js
+++ goodwillfront/src/components/Homepage/Homepage.js
@@ -8,6 +8,33 @@ function Homepage() {
     const [searchQuery, setSearchQuery] = useState('');
     const [companies, setCompanies] = useState(['Goodwill', 'Goodwill', 'JPMC']);
 
+    const [jobs, setJobs] = useState([]); 
+    useEffect(() => {
+        const fetchJobs = () => {
+          fetch('http://127.0.0.1:5000/jobs', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json(); 
+          })
+          .then(data => {
+            setJobs(data); 
+          })
+          .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+          });
+        };
+    
+        fetchJobs(); 
+      }, []); 
+
+    
     // Function to handle new message submissions
     const handleNewMessage = (newMessage) => {
         setMessages([...messages, newMessage]);
@@ -17,14 +44,26 @@ function Homepage() {
     const handleSearchChange = (event) => {
         setSearchQuery(event.target.value);
     };
-    //ethan is that guy
-    const [items] = useState([
-        { id: 1, title:"A.M.P", company:"Goodwill", description:"The purpose of AMP is to help out-of-school young adults between the ages of 18-24 enter a career pathway and attain long-term self-sufficiency prior to program completion.", salaryIncrease:"20k+" },
-        { id: 2, title:"Roads2Work", company:"Goodwill", description:"No previous experience is necessary – just bring the desire to be great! The Roads2Work-Franklin County Commercial Driver Training Program is no cost to you and is funded by a grant from Franklin County Department of Job & Family Services, in partnership with Goodwill Columbus and Capital Transportation Academy.", salaryIncrease:"20k+" },
-        { id: 3, title:"Full Stack Developer", company:"JPMC", description:"JPMC is an amazing company that provides opportunities for young adults to work with nonprofits on fun and exciting projects that have the potential to effect change and create an impact.", salaryIncrease:"30k+" }
-      ]);
+
+    // let [items, setItems] = useState([
+    //     { id: 2, title:"Roads2Work", company:"Goodwill", des:"No previous experience is necessary – just bring the desire to be great! The Roads2Work-Franklin County Commercial Driver Training Program is no cost to you and is funded by a grant from Franklin County Department of Job & Family Services, in partnership with Goodwill Columbus and Capital Transportation Academy.", salaryIncrease:"20k+" },
+    //     { id: 3, title:"Full Stack Developer", company:"JPMC", des:"JPMC is an amazing company that provides opportunities for young adults to work with nonprofits on fun and exciting projects that have the potential to effect change and create an impact.", salary:"30k+" }
+    //   ]);
+
+    // const addItem = newItem => {
+    //     setItems([...items, newItem]);
+    // };
+    // console.log(jobs)
+    // for(let i = 0; i < jobs; i++) {
+    //     // addItem(jobs[i])
+    //     console.log(jobs[i])
+    //     items.push(jobs[i])
+    //     setItems(...items);
+    // }
+
+    // console.log(items)
       //ethan is that guy
-      const filteredItems = items.filter(item =>
+      const filteredItems = jobs.filter(item =>
         item.title.includes(searchQuery)
       );
 
@@ -54,7 +93,7 @@ function Homepage() {
                         <div>
                             {filteredItems.map(item => (
                             <div key={item.id}>
-                                <JobPosting title={item.title} company={item.company} description={item.description} salaryIncrease={item.salaryIncrease}/>
+                                <JobPosting title={item.title} company={item.company} description={item.des} salaryIncrease={item.salary}/>
                             </div>
                             ))}
                         </div>
