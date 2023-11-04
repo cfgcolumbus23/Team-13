2ad2cf45c13ec9613be89f49d2123656fbf332d4
MarkW040 <148275256+MarkW040@users.noreply.github.com>
2023-11-03 20:05:44 -0400

    implemented login in front end to connect to backend

--- goodwillfront/src/components/Loginpage/Loginpage.js
+++ goodwillfront/src/components/Loginpage/Loginpage.js
@@ -1,18 +1,54 @@
 import './Loginpage.css';
+import {useState} from 'react';
 function Loginpage() {
+  const [loginData, setLoginData] = useState({username: "", password: ""});
+  
+    function handleUsernameInput(event) {
+      const {value} = event.target;
+      setLoginData({...loginData, "username":value});
+    }
+
+    function handlePasswordInput(event) {
+      const {value} = event.target;
+      setLoginData({...loginData, "password":value});
+    } 
+    
+    function handleLogin() {
+      fetch('http://localhost:5000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(responseData => {
+        console.log(responseData);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+    }
+
+
     return (
       <div className="login-pane">
         <h1 className='login-label'>User Login</h1>
       <form>
         <div className="input-field">
           <div className='input-label'>Username</div>
-          <input type="text" id="username" />
+          <input type="text" id="username" onChange="handleUsernameInput"/>
         </div>
         <div className="input-field">
         <div className='input-label'>Password</div>
-          <input type="password" id="password" />
+          <input type="password" id="password" onChange="handlePasswordInput"/>
         </div>
-        <button className="login-btn">Login</button>
+        <button className="login-btn" onClick="handleLogin">Login</button>
       </form>
     </div>
     );
