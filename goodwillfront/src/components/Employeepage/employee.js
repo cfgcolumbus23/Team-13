2486c37994086061abdc0a793007ffd1f115d0bc
Hannah-Hoover <hd3611@wayne.edu>
2023-11-04 06:33:14 -0400

    resolving conflicts

--- goodwillfront/src/components/Employeepage/employee.js
+++ goodwillfront/src/components/Employeepage/employee.js
@@ -4,14 +4,8 @@ import './employee.css';
 const Employee = () => {
 
     return (
-<<<<<<< HEAD
-        <div class="content">
-        <div class="General">
-            <h2>Name:</h2>
-=======
         <><div class="General">
             <h2>Name</h2>
->>>>>>> feature-toggle
             <p>Display name</p>
             <h2>Phone:</h2>
             <p>Display phone</p>
@@ -21,23 +15,6 @@ const Employee = () => {
             <p>Display email</p>
             <h2>Employee ID:</h2>
             <p>Display ID</p>
-<<<<<<< HEAD
-        </div>
-        <div class="job">
-            <h2>Job Title:</h2>
-            <p>Display title</p>
-            <h2>Employment startdate:</h2>
-            <p>Display employment start date</p>
-        </div><div class="pay">
-            <h2>Wages:</h2>
-            <p>Display hourly wage</p>
-            <h2>Pay Schedule:</h2>
-            <p>Display pay sched</p>
-            <h2>Hours worked:</h2>
-            <p>Display hours worked in pay period</p>
-        </div></div>
-    )
-=======
         </div><div class="job">
                 <h2>Job Title</h2>
                 <p>Display title</p>
@@ -50,7 +27,6 @@ const Employee = () => {
                 <h2>Hours worked</h2>
                 <p>Display hours worked in pay period</p>
             </div></>  )
->>>>>>> feature-toggle
 }
 
 export default Employee;
