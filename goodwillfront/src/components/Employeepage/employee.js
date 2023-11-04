d5881f316ce0384238384486147ab11d53033621
Hannah-Hoover <hd3611@wayne.edu>
2023-11-04 00:28:57 -0400

    nav and employee css changes

--- goodwillfront/src/components/Employeepage/employee.js
+++ goodwillfront/src/components/Employeepage/employee.js
@@ -5,29 +5,28 @@ const Employee = () => {
 
     return (
         <div class="content">
-        <div class="General"><h2>General</h2>
-            <h3>Name</h3>
+        <div class="General">
+            <h2>Name</h2>
             <p>Display name</p>
-            <h3>Phone</h3>
+            <h2>Phone</h2>
             <p>Display phone</p>
-            <h3>address</h3>
+            <h2>address</h2>
             <p>Display address</p>
-            <h3>email</h3>
+            <h2>email</h2>
             <p>Display email</p>
-            <h3>Employee ID</h3>
+            <h2>Employee ID</h2>
             <p>Display ID</p>
         </div>
-        <div class="job"><h2>Job</h2>
-            <h3>Job Title</h3>
+        <div class="job">
+            <h2>Job Title</h2>
             <p>Display title</p>
-            <h3>Employment startdate</h3>
+            <h2>Employment startdate</h2>
             <p>Display employment start date</p>
         </div><div class="pay">
-            <h3>Wage</h3>
             <p>Display hourly wage</p>
-            <h3>Pay Schedule</h3>
+            <h2>Pay Schedule</h2>
             <p>Display pay sched</p>
-            <h3>Hours worked</h3>
+            <h2>Hours worked</h2>
             <p>Display hours worked in pay period</p>
         </div></div>
     )
