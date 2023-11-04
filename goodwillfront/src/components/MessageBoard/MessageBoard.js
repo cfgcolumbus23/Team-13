411cf2aa400c6bd7272a625e990a178244491ec3
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-11-04 04:10:37 -0400

    added message board

--- goodwillfront/src/components/MessageBoard/MessageBoard.js
+++ goodwillfront/src/components/MessageBoard/MessageBoard.js
@@ -1,95 +1,56 @@
+// File: MessageDisplay.js
+
+import React from 'react';
+import './MessageBoard.css'; // Create a CSS file to style your messages
+import {
+  ChatContainer,
+  MainContainer,
+  Message,
+  MessageGroup,
+  MessageInput,
+  MessageList,
+} from "@chatscope/chat-ui-kit-react";
 import { useState, useEffect } from 'react';
-import { Route, Routes } from "react-router-dom";
 
+const MessageDisplay = () => {
+  const [messages, setMessages] = useState([]); 
 
-import { react } from "@babel/types";
+  useEffect(() => {
+    const fetchMessages = () => {
+      fetch('http://127.0.0.1:5000/messages', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+      })
+      .then(data => {
+        setMessages(data); 
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+    };
 
+    fetchMessages(); 
+  }, []); 
 
-function PostBoard() {
-  const [posts, setPosts] = useState([{username: "", content: "", likes: 0}])
-  const [userPost, setUserPost] = useState({username: "", content: "", likes: 0})
-  const [showForm, setShowForm] = useState(false)
-  const [postsArr, setPostsArr] = useState([{username: "user1", content: "post1", likes: "0"}, {username: "user2", content: "post2", likes: 0}]);
-  function handleInput(event) {
-    const {value} = event.target;
-    setUserPost({...userPost, "content":value})
-  }
-
-
-  function handlePost() {
-    fetch('http://localhost:5000/api/message', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userPost)
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(responseData => {
-      console.log(responseData);
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
-    posts.push(userPost)
-    setPostsArr(...posts)
-    handleHideForm()
-  }
-
-
-  function handleShowForm() {
-    setShowForm(true)
-  }
-  function handleHideForm() {
-    setShowForm(false);
-  }
-
-
-// load the posts array with data from the backend once the component mounts
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/posts')
-  //       .then(response => response.json())
-  //       .then((postData)=> {setPosts(postData)});
-  // }, []);
-
-
-
+  const messageElements = messages.map((message, index) => (
+    <div key={index}>
+      <strong>{message.username}</strong>: {message.content}
+    </div>
+  ));
 
   return (
-    <div style={{ position: "relative", height: "500px" }}>
-        {showForm ? (
-            <div>
-                <form>
-                    <input type="text" onChange={handleInput}></input>
-                </form>
-                <button type="submit" className="btn btn-primary btn-block" onClick={handlePost}>Submit</button>
-            </div>
-        ) : (
-            <div>
-            <ul>
-              {posts.map((post, index) => (
-                <li key={index}>
-                  <strong>Username:</strong> {post.username}
-                  <br />
-                  <strong>Content:</strong> {post.content}
-                  <br />
-                  <strong>Likes:</strong> {post.likes}
-                </li>
-              ))}
-            </ul>
-            <button type="button" onClick={handleShowForm}>Post</button>
-            </div>
-            )
-        }
+    <div>
+      <div id='divbox'>{messageElements}</div>
     </div>
   );
-}
+};
 
-
-export default PostBoard;
-
+export default MessageDisplay;
