c9a7b700467e5630d084a051c8fc3e22ca06757d
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-11-04 00:29:56 -0400

    messages

--- goodwillfront/src/App.js
+++ goodwillfront/src/App.js
@@ -10,6 +10,7 @@ import Employee from './components/Employeepage/employee.js';
 import Home from './components/Homepage/Homepage.js';
 import JobPosting from './components/JobPostings/JobPostings.js';
 import Login from './components/Loginpage/Loginpage.js';
+import MessageDisplay from './components/MessageBoard/MessageBoard';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/messages" element={<MessageDisplay />} />
         <Route path="/job-posting" element={<JobPosting />} />
         <Route path="/employee" element={<Employee />} />
         <Route path="/account" element={<Account />} />
