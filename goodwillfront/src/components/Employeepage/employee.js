bc25539814ec80d144ffac5b3ae393a9a6591ba7
Hannah-Hoover <hd3611@wayne.edu>
2023-11-04 01:35:17 -0400

    employee css

--- goodwillfront/src/components/Employeepage/employee.js
+++ goodwillfront/src/components/Employeepage/employee.js
@@ -6,27 +6,28 @@ const Employee = () => {
     return (
         <div class="content">
         <div class="General">
-            <h2>Name</h2>
+            <h2>Name:</h2>
             <p>Display name</p>
-            <h2>Phone</h2>
+            <h2>Phone:</h2>
             <p>Display phone</p>
-            <h2>address</h2>
+            <h2>Address:</h2>
             <p>Display address</p>
-            <h2>email</h2>
+            <h2>Email:</h2>
             <p>Display email</p>
-            <h2>Employee ID</h2>
+            <h2>Employee ID:</h2>
             <p>Display ID</p>
         </div>
         <div class="job">
-            <h2>Job Title</h2>
+            <h2>Job Title:</h2>
             <p>Display title</p>
-            <h2>Employment startdate</h2>
+            <h2>Employment startdate:</h2>
             <p>Display employment start date</p>
         </div><div class="pay">
+            <h2>Wages:</h2>
             <p>Display hourly wage</p>
-            <h2>Pay Schedule</h2>
+            <h2>Pay Schedule:</h2>
             <p>Display pay sched</p>
-            <h2>Hours worked</h2>
+            <h2>Hours worked:</h2>
             <p>Display hours worked in pay period</p>
         </div></div>
     )
