52486edbf543dedaf82620a6ff12a8019ce86e0f
e-savar <66883712+e-savar@users.noreply.github.com>
2023-11-03 23:23:02 -0400

    wroks

--- goodwillfront/src/components/Accountpage/Navbar/Navbar.js
+++ goodwillfront/src/components/Accountpage/Navbar/Navbar.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './Navbar.css';
-import { Sidebar, Menu, MenuItem} from "react-pro-sidebar";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import { alignProperty } from '@mui/material/styles/cssUtils';
+import React from 'react';
+import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 
 function Navbar() {
@@ -13,9 +14,9 @@ function Navbar() {
 
         <Sidebar style={{ height: "100vh" }}>
             <Menu>
-                <MenuItem icon={<HomeOutlinedIcon />}>Home</MenuItem>
-                <MenuItem icon={<ReceiptOutlinedIcon />}>Messages</MenuItem>
-                <MenuItem icon={<PeopleOutlinedIcon />}>Login</MenuItem>
+                <Link to="/" ><MenuItem icon={<HomeOutlinedIcon />}>Home</MenuItem></Link>
+                <Link to="/messages" ><MenuItem icon={<ReceiptOutlinedIcon />}>Messages</MenuItem></Link>
+                <Link to="/login" ><MenuItem icon={<PeopleOutlinedIcon />}>Login</MenuItem></Link>
             </Menu>
         </Sidebar>
     );
