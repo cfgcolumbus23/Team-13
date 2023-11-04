31a25e0d98769d3e429203de54e3128b059aca95
e-savar <66883712+e-savar@users.noreply.github.com>
2023-11-04 00:17:47 -0400

    added cors fix potentially

--- goodwillfront/src/components/Accountpage/Account.js
+++
@@ -1,9 +0,0 @@
-function Account() {
-    return <div>
-        <div className="account-image">Here is an Image</div>
-        <div className="account-name">First Last</div>
-        <div className="account-username">user.name</div>
-
-    </div>
-}
-export default Account;
