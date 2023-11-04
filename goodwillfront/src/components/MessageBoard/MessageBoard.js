44dac5c400e66e05439202c07430e3e75bad0b6b
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-11-04 03:30:13 -0400

    finished showing messages

--- goodwillfront/src/components/MessageBoard/MessageBoard.js
+++ goodwillfront/src/components/MessageBoard/MessageBoard.js
@@ -10,75 +10,47 @@ import {
   MessageInput,
   MessageList,
 } from "@chatscope/chat-ui-kit-react";
-import { useState } from 'react';
-
-var messages = []
-function getMessages() {
-  fetch('http://127.0.0.1:5000/messages', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    // console.log("RESPONSE")
-    // console.log(response)
-    // console.log(response.json)
-    // // return response.json
-    response.json().then((data) => {
-      // console.log(data);
-      messages = data;
-      console.log(messages)
-      return data;
-  }).catch((err) => {
-      console.log(err);
-  })
-  })
-  .then(responseData => {
-    console.log("RESPONSE DATA")
-    console.log(responseData);
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
-
-}
-
+import { useState, useEffect } from 'react';
 
 const MessageDisplay = () => {
-  // const[messages, setMessages] = useState([{username: string, message: string}]);
-  getMessages();
-  console.log(messages)
-  messages = [{'username': 'robert', 'content': 'This is some dummy data to see the summary!'}]
-  // console.log(messages)
-  // const [message, setMyList] = useState(messages);
-  // setMyList(messages)
-  // console.log("start")
-  // console.log(messages)
-  const messageElements = [];
-for (let i = 0; i < messages.length; i++) {
-  messageElements.push(
-    <div key={i}>
-      <strong>{messages[i].username}</strong>: {messages[i].content}
+  const [messages, setMessages] = useState([]); 
+
+  useEffect(() => {
+    const fetchMessages = () => {
+      fetch('http://127.0.0.1:5000/messages', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+      })
+      .then(data => {
+        setMessages(data); 
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+    };
+
+    fetchMessages(); 
+  }, []); 
+
+  const messageElements = messages.map((message, index) => (
+    <div key={index}>
+      <strong>{message.username}</strong>: {message.content}
     </div>
-  );
-}
+  ));
+
   return (
     <div>
       <div id='divbox'>{messageElements}</div>
-      <div>
-        Message 1
-      </div>
-        
-        <div>
-        By Author
-      </div>
     </div>
   );
 };
-
 
 export default MessageDisplay;
